f8ebb8e4e1be60f69db6e8b404a5a37afbf85c05
captain <cseengineersam@gmail.com>
2020-08-09 21:10:58 +0530

    Updates

---
+++ precache-manifest.eb00922ef445e592510257997216f7b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8b4d0803d065f895dcfbdd96a0ee36",
+    "url": "/temp/index.html"
+  },
+  {
+    "revision": "55a4f9f577f76c54dd94",
+    "url": "/temp/static/js/2.c8f842e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temp/static/js/2.c8f842e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d2150612cc029982f5",
+    "url": "/temp/static/js/main.15b99896.chunk.js"
+  },
+  {
+    "revision": "376896b71fab3f8c9419",
+    "url": "/temp/static/js/runtime-main.f3603f06.js"
+  }
+]);
